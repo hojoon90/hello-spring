0e86d43a533a59247dc679ca06c171befb8868b6
최호준 <hojoon90@gmail.com>
2023-02-05 22:51:39 +0900

    회원 서비스

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,13 +2,10 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -20,7 +17,7 @@ public class MemberService {
     /**
      * 회원 가입
      */
-    public Long join(Member member){
+    public Long join(Member member) {
 
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
@@ -37,11 +34,11 @@ public class MemberService {
     /**
      * 전체 회원 조회
      */
-    public List<Member> findMembers(){
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> findOne(Long memberId){
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 
